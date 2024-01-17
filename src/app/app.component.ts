6b49a1aed2d8b5767532c39210f2eb6dc0c33a93
Henrique Custódia <martinshenrique8@gmail.com>
2024-01-17 20:22:29 -0300

    feat: adiciona componente com singla input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,84 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, effect, input, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterOutlet } from "@angular/router";
+
+function toUpperCase(value: string): string {
+  return value.toUpperCase();
+}
 
 @Component({
-  selector: 'app-root',
+  selector: "app-example",
   standalone: true,
-  imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Optional Input</h1>
+    <div>{{ optionalInput() }}</div>
 
-    <router-outlet />
+    <h1>Optional Input + Initial value</h1>
+    <div>{{ optionalInputWithInitialvalue() }}</div>
+
+    <h1>Optional Input + Initial value + Transform</h1>
+    <div>{{ optionalInputInitialValueWithTransform() }}</div>
+
+    <h1>Optional Input + Initial value + Transform + Alias</h1>
+    <div>{{ optionalInputInitialValueWithAlias() }}</div>
+
+    <hr>
+
+    <h1>Required Input</h1>
+    <div>{{ requiredInput() }}</div>
+
+    <h1>Required Input + Transform</h1>
+    <div>{{ requiredInputWithTransform() }}</div>
+
+    <h1>Required Input + Transform + Alias</h1>
+    <div>{{ requiredInputWithAlias() }}</div>
+
+    <hr>
+    
+    <h1>Computed (Could work like a setter?)</h1>
+    <div>{{ computedValue() }}</div>
+
+  `,
+})
+export class ExampleComponent {
+  optionalInput = input<string>();
+
+  optionalInputWithInitialvalue = input('OK, I have a value');
+
+  optionalInputInitialValueWithTransform = input('OK, I have a value', { transform: toUpperCase });
+  
+  optionalInputInitialValueWithAlias = input('OK, I have a value', { transform: toUpperCase, alias: 'optionalInputWithAlias' });
+
+  requiredInput = input.required();
+
+  requiredInputWithTransform = input.required({ transform: toUpperCase });
+
+  requiredInputWithAlias = input.required({
+    transform: toUpperCase,
+    alias: "withAlias",
+  });
+
+  computedValue = computed(() => `optionalInput + optionalInputInitialValueWithAlias = ${this.optionalInput()} + ${this.optionalInputInitialValueWithAlias()}`)
+}
+
+@Component({
+  selector: "app-root",
+  standalone: true,
+  imports: [ExampleComponent, FormsModule],
+  template: ` 
+    <input  [(ngModel)]="value" />
+
+    <app-example 
+      [optionalInput]="value"
+      [optionalInputWithInitialvalue]="value"
+      [optionalInputInitialValueWithTransform]="value"
+      [optionalInputWithAlias]="value"
+      [requiredInput]="value" 
+      [requiredInputWithTransform]="value" 
+      [withAlias]="value" /> 
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'signal-input';
+  value = 'test';
 }
